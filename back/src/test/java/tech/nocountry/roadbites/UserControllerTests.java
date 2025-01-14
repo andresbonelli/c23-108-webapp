c013bb3b6f684419efef56ed35568259f2479243
andresbonelli <andresbonellipiano@gmail.com>
2025-01-14 18:51:30 -0300

    User CRUD: UserControllertests

--- back/src/test/java/tech/nocountry/roadbites/UserControllerTests.java
+++ back/src/test/java/tech/nocountry/roadbites/UserControllerTests.java
@@ -1,16 +1,33 @@
 package tech.nocountry.roadbites;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tech.nocountry.roadbites.controller.UserController;
 import tech.nocountry.roadbites.controller.dto.RegisterUserDto;
+import tech.nocountry.roadbites.domain.model.User;
+import tech.nocountry.roadbites.domain.repository.UserRepository;
 
 @SpringBootTest
 public class UserServiceTests {
 
     @Autowired
     private UserController userController;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setup() {
+        userRepository.deleteAll();
+        userRepository.save(new User(
+                "testuser1"
+        ))
+
+
+    }
 
     @Test
     void shouldCreateNewUser() {
