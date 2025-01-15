64cc06fe303c1f81848bfae38276ce24c1056810
Javier Marquez <hello.jmarquez@gmail.com>
2025-01-15 08:30:48 -0500

    home site agregado

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				handwritten: ['"Just Another Hand"', 'cursive'], // Define la nueva fuente
+			},
+		},
 	},
 	plugins: [],
 };
