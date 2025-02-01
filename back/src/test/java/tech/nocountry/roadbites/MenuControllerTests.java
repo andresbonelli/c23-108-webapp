f306a3afe2e06419796c6d9bdd02d13a99e4f315
andresbonelli <andresbonellipiano@gmail.com>
2025-02-01 15:48:05 -0300

    Refactor Tests with Test Containers

---
+++ back/src/test/java/tech/nocountry/roadbites/MenuControllerTests.java
@@ -0,0 +1,73 @@
+package tech.nocountry.roadbites;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.hamcrest.Matchers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MenuControllerTests {
+
+    @ServiceConnection
+    static final MySQLContainer mySqlContainer = new MySQLContainer("mysql:8.0.26")
+            .withDatabaseName("roadbites")
+            .withUsername("user")
+            .withPassword("password");
+
+    static {
+        mySqlContainer.start();
+    }
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        mySqlContainer.stop();
+    }
+
+    @Test
+    void shouldGetAllMenus() {
+        RestAssured.given()
+                .when()
+                .get("/api/menu")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("size()", Matchers.greaterThan(0))
+                .body("[0].id", Matchers.equalTo(1))
+                .body("[0].name", Matchers.equalTo("Arroz con Pollo"));
+    }
+
+    @Test
+    void shouldGetMenusByCategory() {
+        RestAssured.given()
+                .when()
+                .get("/api/menu/category/bebidas")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("size()", Matchers.greaterThan(0))
+                .body("[0].category", Matchers.equalTo("bebidas"));
+
+        RestAssured.given()
+                .when()
+                .get("/api/menu/category/snacks")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("size()", Matchers.greaterThan(0))
+                .body("[0].category", Matchers.equalTo("snacks"));
+    }
+}
