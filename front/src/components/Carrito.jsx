939cade9d82860cd6ad9339d5caf6fe4778f81d4
Javier Marquez <hello.jmarquez@gmail.com>
2025-02-01 19:24:11 -0500

    estilos agregados a carrito

--- front/src/components/Carrito.jsx
+++ front/src/components/Carrito.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+/* eslint-disable react/prop-types */
+
 import { GrSubtractCircle, GrAddCircle } from 'react-icons/gr';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Carrito = ({ cartItems, setCartItems }) => {
 	const increaseQuantity = itemId => {
@@ -40,42 +42,53 @@ const Carrito = ({ cartItems, setCartItems }) => {
 			<h2 className="text-6xl font-bold text-center mb-8 text-emerald-950  py-4 font-handwritten">
 				Carrito de Compras
 			</h2>
-			<div className="grid gap-6">
-				{cartItems.map(item => (
-					<div
-						key={item.id}
-						className="border bg-slate-50 rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow"
-					>
-						<div className="flex items-center gap-4">
-							<div className="w-24 h-24">
-								<img
-									src={item.img}
-									alt={item.nombre}
-									className="w-full h-full object-cover rounded-lg"
-								/>
-							</div>
-							<div className="flex-1">
-								<h3 className="text-xl font-semibold">{item.nombre}</h3>
-								<p className="text-gray-600">{item.descripcion}</p>
-
-								<div className="flex items-center gap-2">
-									<button onClick={() => decreaseQuantity(item.id)}>
-										<GrSubtractCircle className="text-red-600" />
-									</button>
-									<span className="text-lg font-medium w-8 text-center">
-										{item.quantity || 1}
-									</span>
-									<button onClick={() => increaseQuantity(item.id)}>
-										<GrAddCircle className="text-green-600" />
-									</button>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 0.5 }}
+				className="grid gap-6"
+			>
+				<AnimatePresence>
+					{cartItems.map(item => (
+						<motion.div
+							key={item.id}
+							initial={{ opacity: 0, y: -20 }}
+							animate={{ opacity: 1, y: 0 }}
+							exit={{ opacity: 0, y: -20 }}
+							transition={{ duration: 0.3 }}
+							className="border bg-slate-50 rounded-lg p-4 shadow-md hover:shadow-lg"
+						>
+							<div className="flex items-center gap-4">
+								<div className="w-24 h-24">
+									<img
+										src={item.img}
+										alt={item.nombre}
+										className="w-full h-full object-cover rounded-lg"
+									/>
 								</div>
+								<div className="flex-1">
+									<h3 className="text-xl font-semibold">{item.nombre}</h3>
+									<p className="text-gray-600">{item.descripcion}</p>
+
+									<div className="flex items-center gap-2">
+										<button onClick={() => decreaseQuantity(item.id)}>
+											<GrSubtractCircle className="text-red-600" />
+										</button>
+										<span className="text-lg font-medium w-8 text-center">
+											{item.quantity || 1}
+										</span>
+										<button onClick={() => increaseQuantity(item.id)}>
+											<GrAddCircle className="text-green-600" />
+										</button>
+									</div>
+								</div>
+								<p className="text-lg font-bold text-green-600">
+									Precio: ${(item.precio * (item.quantity || 1)).toFixed(2)}
+								</p>
 							</div>
-							<p className="text-lg font-bold text-green-600">
-								Precio: ${(item.precio * (item.quantity || 1)).toFixed(2)}
-							</p>
-						</div>
-					</div>
-				))}
+						</motion.div>
+					))}
+				</AnimatePresence>
 
 				{cartItems.length === 0 ? (
 					<p className="text-center text-emerald-600 bg-white p-4 rounded-lg">
@@ -88,7 +101,7 @@ const Carrito = ({ cartItems, setCartItems }) => {
 						</p>
 					</div>
 				)}
-			</div>
+			</motion.div>
 		</div>
 	);
 };
